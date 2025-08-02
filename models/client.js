ca539d0277b2850a4e03f5c222af6c9f3c6c462d
Karthikv618 <karthikyashodha424@gmail.com>
2025-08-02 10:41:32 +0530

    Add files via upload

---
+++ models/client.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const ClientSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+ company:{ type: String},
+ requirment: { type: String}
+});
+
+module.exports = mongoose.model('Client', ClientSchema);
---
+++ models/freelancer.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const FreelancerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  skills: [String],
+  password:{type:String, required:true, unique: true}
+});
+
+module.exports = mongoose.model('Freelancer', FreelancerSchema);
